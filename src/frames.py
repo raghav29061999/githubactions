04c8004d743860f7896134568040f27b90dd7035
Raghav <raghav@gmail.com>
2025-05-19 15:53:40 +0530

    few more changes_2

--- src/frames.py
+++ src/frames.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def create_dataframe(list_1, list_2, list_3) -> pd.DataFrame:
     
-    df = pd.DataFrame({'Col_A': list_1, 'Col_B':list_2, 'Col_C':list_3})
+    df = pd.DataFrame({'Col_D': list_1, 'Col_B':list_2, 'Col_C':list_3})
 
 
     return df
