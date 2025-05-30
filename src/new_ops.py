7e793ff0d2011fc7d73aa604d5f521b72ab0cfff
Raghav <raghav@gmail.com>
2025-05-30 20:16:06 +0530

    made changes from pep8

--- src/new_ops.py
+++ src/new_ops.py
@@ -1,13 +1,17 @@
 import math
 
-def pow(a,b):
+
+def pow(a, b):
     return a ** b
 
-def log_func(a,b):
-    return math.log(a,b)
+
+def log_func(a, b):
+    return math.log(a, b)
+
 
 def trig_funcs(angle):
     return math.sin(angle), math.cos(angle)
+
 
 def print_func():
     print("yeh tob print hoga")
--- tests/test_new_ops.py
+++ tests/test_new_ops.py
@@ -3,10 +3,12 @@ import math
 import pytest
 from src.new_ops import pow, log_func, trig_funcs
 
+
 def test_pow():
     assert pow(2, 3) == 8
     assert pow(5, 0) == 1
     assert pow(9, 0.5) == 3
+
 
 def test_log_func():
     assert log_func(8, 2) == pytest.approx(3)
@@ -19,15 +21,16 @@ def test_log_func():
     with pytest.raises(ValueError):
         log_func(10, -2)  # negative base is invalid
 
+
 def test_trig_funcs():
     angle = math.pi / 4  # 45 degrees in radians
-    sin_val, cos_val= trig_funcs(angle)
+    sin_val, cos_val = trig_funcs(angle)
     assert sin_val == pytest.approx(math.sqrt(2)/2)
     assert cos_val == pytest.approx(math.sqrt(2)/2)
     # assert tan_val == pytest.approx(1)
 
     angle = 0
-    sin_val, cos_val= trig_funcs(angle)
+    sin_val, cos_val = trig_funcs(angle)
     assert sin_val == pytest.approx(0)
     assert cos_val == pytest.approx(1)
     # assert tan_val == pytest.approx(0)
