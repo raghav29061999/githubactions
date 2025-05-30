407bb32e930c3ebc22ea93531424b3ea5d02a3d3
Raghav <raghav@gmail.com>
2025-05-30 20:07:18 +0530

    i made changes using black

--- tests/test_math_operations.py
+++ tests/test_math_operations.py
@@ -5,17 +5,16 @@ from src.math_operations import add, subtract, divide
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 5) == 4
-    
+
 
 def test_subtract():
     assert subtract(10, 5) == 5
     assert subtract(0, 3) == -3
-    
+
 
 def test_divide():
     assert divide(10, 2) == 5
     assert divide(9, 3) == 3
-    
 
     # Test divide by zero error
     with pytest.raises(ZeroDivisionError):
