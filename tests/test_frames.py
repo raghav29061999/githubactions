afd3ca91ddeebe4ecf909dff054caeb2b740e559
Raghav <raghav@gmail.com>
2025-05-30 21:20:29 +0530

    all changes_1

--- tests/test_frames.py
+++ tests/test_frames.py
@@ -6,6 +6,5 @@ from src.frames import create_dataframe
 
 def test_frame():
     assert (
-        type(create_dataframe([1, 2, 3, 4], [
-             5, 4, 6, 7], [1, 4, 3, 2])) == pd.DataFrame
+        type(create_dataframe([1, 2, 3, 4], [5, 4, 6, 7], [1, 4, 3, 2])) == pd.DataFrame
     )
