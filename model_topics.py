f6d4476f1bb9d9fd5aef1ac74f964ffcaa59ab7f
raghav29061999 <81700992+raghav29061999@users.noreply.github.com>
2025-08-06 17:42:47 +0530

    Update model_topics.py

--- model_topics.py
+++ model_topics.py
@@ -1,8 +1,11 @@
-from collections import Counter
-
 def generate_super_cluster_labels(df, cluster_col='super_cluster', label_col='cluster_label', top_n=3):
     super_cluster_labels = {}
     for super_cluster_id in df[cluster_col].unique():
+        if super_cluster_id == -1:
+            # Keep unclustered as a fixed label
+            super_cluster_labels[super_cluster_id] = "Unclustered"
+            continue
+        
         sub_labels = df[df[cluster_col] == super_cluster_id][label_col].tolist()
         # Combine all labels into one text
         combined_text = ' '.join(sub_labels).lower()
